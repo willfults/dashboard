bbdb10bcb518b1cf7cbfc475979ebeace72bfe1d
Will Fults <wfults@callsource.com>
2013-06-05 13:30:23 -0700

    fullscreen button functionality added

--- assets/javascripts/layout.js
+++ assets/javascripts/layout.js
@@ -3,5 +3,12 @@ $(document).ready(function() {
 	$('#save-gridster').click(function(){
       	$.cookie('layout', $('#gridster-code').val(), { expires: 7000, path: '/'  });
     });
+    
+    $('#fullscreen').click(function(){
+    	$(document).fullScreen(true);
+    });
+    
+    //http://gridster.net/
+    var gridster = $(".gridster ul").gridster().data('gridster');
 	
 });
