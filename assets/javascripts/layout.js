7de0fd967ea2df25b2bdc7f138e0284eedb3359c
Will Fults <wfults@callsource.com>
2013-06-06 09:27:39 -0700

    clean up UI, add bootstrap dropdown for actions

--- assets/javascripts/layout.js
+++ assets/javascripts/layout.js
@@ -15,5 +15,10 @@ $(document).ready(function() {
 	 var gridsters = $(".gridster ul").gridster().data('gridster');
 	 gridsters.remove_widget( $('#' + id) );
  }
+ 
+ function addWidget(){
+ 	 var gridstered = $(".gridster ul").gridster().data('gridster');
+ 	gridstered.add_widget('<li class="new" style="background-color: black">A new example widget, draggable...</li>', 1, 1, 1, 1);
+ }
 
 
